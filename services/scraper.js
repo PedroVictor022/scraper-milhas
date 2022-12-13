535fab24ef9e6ba57b995ec09169b5fe8c884b8b
Pedro Victor <pedro20002victor@gmail.com>
2022-12-12 22:35:30 -0300

    update return tableValues

--- services/scraper.js
+++ services/scraper.js
@@ -1,8 +1,57 @@
+const e = require('express');
+const pup = require('puppeteer');
+
 const getMilhasDetails = async () => {
   try {
 
-    return'TESTE 1, 2, 3 TESTE';
-  } catch(err) {
+    let _htmlInfo = [];
+    let _htmlValue = [];
+    let row = {};
+
+    const browser = await pup.launch({
+      headless: true,
+    });
+
+    const page = await browser.newPage();
+    console.log('Browser init');
+
+    await page.goto('https://www.e-milhas.com/cotacao');
+
+    await page.waitForSelector("#tabela-cotacao");
+
+    const tableMilhas = await page.$$eval('#tabela-cotacao > tbody', el => el.map((content) => content.innerText))
+    _htmlInfo.push(tableMilhas)
+
+    const dateAtUpdate = await page.$$eval('body > div:nth-child(15) > div > div > div.col-md-8.col-sm-12 > div:nth-child(1) > div > div.col > p.mb-0.mt-4', el => el.map((content) => content.textContent));
+
+    _htmlValue.push({
+      label: 'Updated',
+      value: dateAtUpdate
+    });
+
+    /**
+     * TODO 
+     * 1 - Fazer verificacao se a string retornada tem R$
+     * 2 - Se tiver adiciona no array
+     * 3 - Pensar em logica pra filtrar
+     */
+
+    const tableValues = await page.$$eval('#tabela-cotacao > tbody > tr > td:nth-child(2)', el => el.map((content) => {
+      if(content.innerText.includes('R$')) {
+        return content.innerText;
+      }
+    }))
+    _htmlValue.push({
+      label: 'Milha values',
+      value: tableValues
+    })
+
+
+
+    return {
+      _htmlValue
+    }
+  } catch (err) {
     return `OOPS: ${err}`;
   };
 };
---
+++ services/testeDeString.js
@@ -0,0 +1,7 @@
+let text = 'R$ 99.90'
+
+if(text.includes('R$')) {
+  console.log('Tem cifrao');
+} else {
+  console.log('Nao tem cifrao');
+};
